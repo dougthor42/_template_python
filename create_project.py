5b8ba93c8ef4b028cc5755f6e4b9f2d7ba454927
Douglas Thor <dthor@transphormusa.com>
2020-02-27 15:38:06 -0800

    Update docstring for create_project.py

--- create_project.py
+++ create_project.py
@@ -10,6 +10,13 @@ from cookiecutter.main import cookiecutter
 @click.command()
 @click.argument("outdir", type=click.Path(exists=False, file_okay=False))
 def main(outdir):
+    """
+    Create a new project in OUTDIR.
+
+    Note that OUTDIR should *not* contain the project name - CookieCutter
+    will create the project directory automatically.
+    """
+
     cookiecutter(
         template=".",
         extra_context={"create_date": datetime.date.today().isoformat()},
